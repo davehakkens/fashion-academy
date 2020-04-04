271d04b802c2edfffdb51f9046d46ccaf12c4c0c
Dave Hakkens <dave@davehakkens.nl>
2020-04-04 13:00:41 +0200

    update github name

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -34,7 +34,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'fashion-academy',
-  organizationName: 'ONEARMY',
+  organizationName: 'davehakkens',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
